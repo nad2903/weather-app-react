6314e1ec103e30a38b997f014680f056e83a0bcf
nad2903 <77510852+nad2903@users.noreply.github.com>
2021-03-21 13:49:27 -0400

    implemented real weather data

--- src/Weather.js
+++ src/Weather.js
@@ -1,101 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  function handleResponse(response) {
-    console.log(response);
-  }
-
+  const [weatherData, setWeatherData] = useState({loaded: false});
   const apiKey = "b3a63df8beace64c182e708181f083a8";
   let city = "Toronto"
   let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-  axios.get(apiUrl).then(handleResponse);
 
+  function handleResponse(response) {
+    setWeatherData({
+      temp: response.data.main.temp,
+      description: response.data.weather[0].description,
+      feelsLike: response.data.main.feels_like,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      loaded: true
+    })
+  }
 
-  return (
-    <div className="container">
-      <div className="weather">
-        <form id="search-engine">
-          <div className="row">
-            <div className="col-9">
-              <div className="input-group mb-3">
+  if (weatherData.loaded) {
+    return (
+      <div className="container">
+        <div className="weather">
+          <form id="search-engine">
+            <div className="row">
+              <div className="col-9">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search another city..."
+                    aria-describedby="basic-addon2"
+                    id="search-city-input"
+                    autoComplete="off"
+                    autoFocus="on"
+                  />
+                </div>
+              </div>
+              <div className="col-3">
                 <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search another city..."
-                  aria-describedby="basic-addon2"
-                  id="search-city-input"
-                  autoComplete="off"
-                  autoFocus="on"
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
                 />
               </div>
             </div>
+          </form>
+          <div className="row">
+            <span id="current-time">Last updated at 05:52 PM</span>
+          </div>
+          <div className="row">
+            <h1 className="city" id="city">
+              {city}
+            </h1>
+          </div>
+          <div className="row">
+            <p className="today-date-time">
+              {" "}
+              <br />
+              <span id="today-date">Monday March 8</span>
+            </p>
+          </div>
+          <div className="row">
+            <div className="col-3"></div>
             <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                id="current-icon"
               />
             </div>
-          </div>
-        </form>
-        <div className="row">
-          <span id="current-time">Last updated at 05:52 PM</span>
-        </div>
-        <div className="row">
-          <h1 className="city" id="city">
-            Toronto
-          </h1>
-        </div>
-        <div className="row">
-          <p className="today-date-time">
-            {" "}
-            <br />
-            <span id="today-date">Monday March 8</span>
-          </p>
-        </div>
-        <div className="row">
-          <div className="col-3"></div>
-          <div className="col-3">
-            <img
-              src="http://openweathermap.org/img/wn/10d@2x.png"
-              alt="Weather condition"
-              id="current-icon"
-            />
-          </div>
-          <div className="col-6">
-            <div className="row">
-              <div className="col-3">
-                <h5 className="today-temp" id="today-temp">
-                  6º
-                </h5>
-              </div>
-              <div className="col">
-                <span id="unit-links">
-                  <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm#" id="celsius" className="active">
-                    ºC
-                  </a>{" "}
-                  / {" "}
-                  <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm" id="fahrenheit">
-                    ºF
-                  </a>
-                </span>
+            <div className="col-6">
+              <div className="row">
+                <div className="col-3">
+                  <h5 className="today-temp" id="today-temp">
+                    {Math.round(weatherData.temp)}
+                  </h5>
+                </div>
+                <div className="col">
+                  <span id="unit-links">
+                    <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm#" id="celsius" className="active">
+                      ºC
+                    </a>{" "}
+                    / {" "}
+                    <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm" id="fahrenheit">
+                      ºF
+                    </a>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <p className="today-stats">
-            <span id="weather-condition">Weather</span> <br />
-            Feels like: <span id="feels-like">2º</span>
-            <br />
-            Humidity: <span id="humidity">61%</span> <br />
-            Wind speed: <span id="wind-speed">16 km/h </span>
-          </p>
+          <div className="row">
+            <p className="today-stats">
+              <span className="text-capitalize" id="weather-condition">{weatherData.description}</span> <br />
+              Feels like: <span id="feels-like">{Math.round(weatherData.feelsLike)}º</span>
+              <br />
+              Humidity: <span id="humidity">{Math.round(weatherData.humidity)}%</span> <br />
+              Wind speed: <span id="wind-speed">{Math.round(weatherData.wind)} km/h </span>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    
+    axios.get(apiUrl).then(handleResponse);
+
+    return ("Loading...");
+  }
 }
