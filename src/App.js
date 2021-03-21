8abbd88127dd7f2b802ffbf85a1398734b047ba5
nad2903 <77510852+nad2903@users.noreply.github.com>
2021-03-21 12:55:15 -0400

    Updated bootstrap implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Weather from "./Weather";
 import Link from "./Link";
 
+import "bootstrap/dist/css/bootstrap.css";
+
 export default function App() {
   return (
     <div className="App">
--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.css";
 import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="weather">
-      <div className="row">
+    <div className="container">
+      <div className="weather">
         <form id="search-engine">
           <div className="row">
             <div className="col-9">
@@ -17,7 +17,7 @@ export default function Weather() {
                   aria-label="Recipient's username"
                   aria-describedby="basic-addon2"
                   id="search-city-input"
-                  autocomplete="off"
+                  autoComplete="off"
                   autoFocus="on"
                 />
               </div>
@@ -31,60 +31,60 @@ export default function Weather() {
             </div>
           </div>
         </form>
-      </div>
-      <div className="row">
-        <span id="current-time">Last updated at 05:52 PM</span>
-      </div>
-      <div className="row">
-        <h1 className="city" id="city">
-          Toronto
-        </h1>
-      </div>
-      <div className="row">
-        <p className="today-date-time">
-          {" "}
-          <br />
-          <span id="today-date">Monday March 8</span>
-        </p>
-      </div>
-      <div className="row">
-        <div className="col-3"></div>
-        <div className="col-3">
-          <img
-            src="http://openweathermap.org/img/wn/10d@2x.png"
-            alt="Weather condition"
-            id="current-icon"
-          />
+        <div className="row">
+          <span id="current-time">Last updated at 05:52 PM</span>
         </div>
-        <div className="col-6">
-          <div className="row">
-            <div className="col-3">
-              <h5 className="today-temp" id="today-temp">
-                6º
-              </h5>
-            </div>
-            <div className="col">
-              <span id="unit-links">
-                <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm#" id="celsius" className="active">
-                  ºC
-                </a>{" "}
-                / {" "}
-                <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm" id="fahrenheit">
-                  ºF
-                </a>
-              </span>
+        <div className="row">
+          <h1 className="city" id="city">
+            Toronto
+          </h1>
+        </div>
+        <div className="row">
+          <p className="today-date-time">
+            {" "}
+            <br />
+            <span id="today-date">Monday March 8</span>
+          </p>
+        </div>
+        <div className="row">
+          <div className="col-3"></div>
+          <div className="col-3">
+            <img
+              src="http://openweathermap.org/img/wn/10d@2x.png"
+              alt="Weather condition"
+              id="current-icon"
+            />
+          </div>
+          <div className="col-6">
+            <div className="row">
+              <div className="col-3">
+                <h5 className="today-temp" id="today-temp">
+                  6º
+                </h5>
+              </div>
+              <div className="col">
+                <span id="unit-links">
+                  <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm#" id="celsius" className="active">
+                    ºC
+                  </a>{" "}
+                  / {" "}
+                  <a href="https://calculator-converter.com/celsius-to-fahrenheit.htm" id="fahrenheit">
+                    ºF
+                  </a>
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <p className="today-stats">
-          <span id="weather-condition">Weather</span> <br />
-          Feels like: <span id="feels-like">2º</span>
-          <br />
-          Humidity: <span id="humidity">61%</span> <br />
-          Wind speed: <span id="wind-speed">16 km/h </span>
-        </p>
+        <div className="row">
+          <p className="today-stats">
+            <span id="weather-condition">Weather</span> <br />
+            Feels like: <span id="feels-like">2º</span>
+            <br />
+            Humidity: <span id="humidity">61%</span> <br />
+            Wind speed: <span id="wind-speed">16 km/h </span>
+          </p>
+        </div>
       </div>
     </div>
   );
