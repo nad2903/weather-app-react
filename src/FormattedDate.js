6dc5c483ec5e8afc5a86fb32bb27839078fb3e0a
nad2903 <77510852+nad2903@users.noreply.github.com>
2021-04-05 20:39:51 -0400

    Fixed warnings

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -4,8 +4,8 @@ export default function FormattedDate(props) {
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     return (
-        <div className="FormattedDate">
+        <span className="FormattedDate">
             {days[props.data.getDay()]} {months[props.data.getMonth()]} {props.data.getDate()}
-        </div>
+        </span>
     );
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,6 +26,8 @@ export default function WeatherForecast(props) {
                     <Forecast data={dailyForecast} />
                   </div>
                 );
+              } else {
+                return null;
               }
             })}
           </div>
